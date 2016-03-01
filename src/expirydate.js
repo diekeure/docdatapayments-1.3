002856070ebe3560530e334de221bae2f6fb66e8
Bert Casier <bert@ernie.be>
2016-03-01 14:35:29 +0100

    clean up expiry date
    make them attributes

--- src/expirydate.js
+++ src/expirydate.js
@@ -1,6 +1,34 @@
 class ExpiryDate{
-    month = null;
-    year = null;
+    attributes = {
+        month: null,
+        year: null
+    }
+
+    constructor(month, year){
+        "use strict";
+        this.month = month;
+        this.year = year;
+    }
+
+    get month(){
+        "use strict";
+        return this.attributes.month;
+    }
+
+    set month(val){
+        "use strict";
+        this.attributes.month = val;
+    }
+
+    get year(){
+        "use strict";
+        return this.attributes.year;
+    }
+
+    set year(val){
+        "use strict";
+        this.attributes.year = val;
+    }
 }
 
 
